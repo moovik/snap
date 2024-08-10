8d25d4e1e484fd9d2797d52daa51db0161a55f70
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-08-10 16:57:35 +0300

    Create snapinter.sh

---
+++ snapinter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#export LC_NUMERIC="en_US.UTF-8"
+if [ -n "$1" ]
+then
+ pabkey=$1
+else
+ echo "NO TRASTED PABKEY"
+ exit
+fi
+echo $pabkey
+ip=0
+ip=$(solana gossip | grep $pabkey | awk '{print $1}')":8899"
+echo $ip
+#exit
+wget --trust-server-names -P /mnt/disk1/snapshots/ http://$ip/snapshot.tar.bz2
+wget --trust-server-names -P /mnt/disk1/snapshots/ http://$ip/incremental-snapshot.tar.bz2
