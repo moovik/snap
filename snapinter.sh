d3725cce77eee43f9d4685c468db98d6eddee5b0
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-08-10 17:28:30 +0300

    Update snapinter.sh

--- snapinter.sh
+++ snapinter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --trust-server-names -P /mnt/disk1/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
-wget --trust-server-names -P /mnt/disk1/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
+wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
