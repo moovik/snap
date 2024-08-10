e09a3b306f5ebcf20866a386892d2dbaf72f3d05
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-08-10 20:08:43 +0300

    Update snapintold.sh

--- snapintold.sh
+++ snapintold.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#export LC_NUMERIC="en_US.UTF-8"
-if [ -n "$1" ]
-then
- pabkey=$1
-else
- echo "NO TRASTED PABKEY"
- exit
-fi
-echo $pabkey
-ip=0
-ip=$(solana gossip | grep $pabkey | awk '{print $1}')":8899"
-echo $ip
-#exit
-wget --trust-server-names -P /mnt/disk1/snapshots http://$ip/snapshot.tar.bz2
-wget --trust-server-names -P /mnt/disk1/snapshots http://$ip/incremental-snapshot.tar.bz2
+case "$1" in
+  "1")
+    wget --trust-server-names -P /mnt/disk1/snapshots http://api.mainnet-beta.solana.com/snapshot.tar.bz2
+    ;;
+  "2")
+    wget --trust-server-names -P /mnt/disk1/snapshots http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+    ;;
+  *)
+    wget --trust-server-names -P /mnt/disk1/snapshots http://api.mainnet-beta.solana.com/snapshot.tar.bz2
+    wget --trust-server-names -P /mnt/disk1/snapshots http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+    ;;
+esac
