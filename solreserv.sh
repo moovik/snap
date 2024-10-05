1df7082f45d8fc22e0a3eb142233a8c5e23ca788
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-10-05 15:47:18 +0300

    Create solreserv.sh

---
+++ solreserv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --trust-server-names -P /data2/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
+wget --trust-server-names -P /data2/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
