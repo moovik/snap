3d61214325b0164314f72a3d802170829367307c
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-10-22 22:15:52 +0300

    Update solreserv.sh

--- solreserv.sh
+++ solreserv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget --trust-server-names -P /data2/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
-wget --trust-server-names -P /data2/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
+wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+systemctl daemon-reload && systemctl restart solana.service
