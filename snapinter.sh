3564ac2dd4a22323dec8ba5559dfe1fdfa95e09e
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-11-07 16:53:17 +0300

    Update snapinter.sh

--- snapinter.sh
+++ snapinter.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget --trust-server-names -P /mnt/disk1/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
 wget --trust-server-names -P /mnt/disk1/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
+systemctl daemon-reload && systemctl restart solana.service
+agave-validator --ledger /root/solana/ledger monitor
