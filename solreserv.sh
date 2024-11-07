4ea90d3403ed0417e00376e4dc4112307e93e5ef
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-11-07 10:10:19 +0300

    Update solreserv.sh

--- solreserv.sh
+++ solreserv.sh
@@ -2,4 +2,4 @@
 wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/snapshot.tar.bz2
 wget --trust-server-names -P /root/solana/snapshots/ http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2
 systemctl daemon-reload && systemctl restart solana.service
-solana-validator --ledger /root/solana/ledger monitor
+agave-validator --ledger /root/solana/ledger monitor
