6e23bf6ca532da1335769f21801d50098a6b5fcd
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-11-28 20:26:49 +0300

    Update solreserv.sh

--- solreserv.sh
+++ solreserv.sh
@@ -4,13 +4,16 @@
 SNAPSHOT_URL="http://api.mainnet-beta.solana.com/snapshot.tar.bz2"
 INCREMENTAL_SNAPSHOT_URL="http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2"
 TARGET_DIR="/root/solana/snapshots/"
+SNAPSHOT_MASK="snapshot-*.tar.*"
+INCREMENTAL_SNAPSHOT_MASK="incremental-snapshot-*.tar.*"
 
-# Функция для загрузки файла с повторными попытками
+# Функция для загрузки файла с проверкой
 download_file() {
     local url="$1"
     local target_dir="$2"
     local success=0
 
+    # Загрузка с повторными попытками
     while [ $success -eq 0 ]; do
         wget --trust-server-names -P "$target_dir" "$url"
         if [ $? -eq 0 ]; then
@@ -23,16 +26,24 @@ download_file() {
     done
 }
 
-# Скачивание первого файла
-echo "Пытаемся скачать основной снимок..."
-download_file "$SNAPSHOT_URL" "$TARGET_DIR"
-
-# Если первый файл успешно скачан, пробуем скачать второй
-if [ $? -eq 0 ]; then
-    echo "Основной снимок успешно скачан. Переходим к загрузке инкрементального снимка..."
-    download_file "$INCREMENTAL_SNAPSHOT_URL" "$TARGET_DIR"
+# Проверяем, существует ли основной снимок
+if ls "${TARGET_DIR}${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+    echo "Основной снимок уже существует."
 else
-    echo "Не удалось скачать основной снимок. Загрузка инкрементального снимка отменена."
+    echo "Основной снимок отсутствует. Начинаем загрузку..."
+    download_file "$SNAPSHOT_URL" "$TARGET_DIR"
+fi
+
+# Если основной снимок существует, переходим к загрузке инкрементального снимка
+if ls "${TARGET_DIR}${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+    echo "Переходим к загрузке инкрементального снимка..."
+    if ls "${TARGET_DIR}${INCREMENTAL_SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+        echo "Инкрементальный снимок уже существует."
+    else
+        download_file "$INCREMENTAL_SNAPSHOT_URL" "$TARGET_DIR"
+    fi
+else
+    echo "Основной снимок отсутствует. Инкрементальный снимок загружаться не будет."
     exit 1
 fi
 
