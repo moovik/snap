c6f37f1e1f8ded7f221a74a929368e86abe1c04b
Vitaliy M. <9020849+moovik@users.noreply.github.com>
2024-11-28 20:34:17 +0300

    Update solreserv.sh

--- solreserv.sh
+++ solreserv.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
 # Переменные
-SNAPSHOT_URL="http://api.mainnet-beta.solana.com/snapshot.tar.bz2"
-INCREMENTAL_SNAPSHOT_URL="http://api.mainnet-beta.solana.com/incremental-snapshot.tar.bz2"
-TARGET_DIR="/root/solana/snapshots/"
+API_URL="http://api.mainnet-beta.solana.com"
 SNAPSHOT_MASK="snapshot-*.tar.*"
 INCREMENTAL_SNAPSHOT_MASK="incremental-snapshot-*.tar.*"
+TARGET_DIR="$HOME/solana/snapshots"
 
-# Функция для загрузки файла с проверкой
+# Проверяем, передан ли pubkey
+if [ -n "$1" ]; then
+    pabkey=$1
+else
+    echo "NO TRUSTED PUBKEY PROVIDED"
+    exit 1
+fi
+
+echo "Используемый pubkey: $pabkey"
+
+# Пытаемся найти IP-адрес по pubkey
+ip=$(solana gossip | grep $pabkey | awk '{print $1}')
+if [ -n "$ip" ]; then
+    ip="$ip:8899"
+    echo "IP-адрес узла: $ip"
+else
+    echo "Не удалось найти узел по указанному pubkey. Будет использован основной API."
+    ip="$API_URL"
+fi
+
+# Функция для загрузки файла с повторными попытками
 download_file() {
     local url="$1"
     local target_dir="$2"
     local success=0
 
-    # Загрузка с повторными попытками
     while [ $success -eq 0 ]; do
         wget --trust-server-names -P "$target_dir" "$url"
         if [ $? -eq 0 ]; then
@@ -26,25 +44,25 @@ download_file() {
     done
 }
 
-# Проверяем, существует ли основной снимок
-if ls "${TARGET_DIR}${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+# Проверяем наличие основного снимка
+if ls "${TARGET_DIR}/${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
     echo "Основной снимок уже существует."
 else
-    echo "Основной снимок отсутствует. Начинаем загрузку..."
-    download_file "$SNAPSHOT_URL" "$TARGET_DIR"
+    echo "Начинаем загрузку основного снимка..."
+    download_file "http://$ip/snapshot.tar.bz2" "$TARGET_DIR"
 fi
 
-# Если основной снимок существует, переходим к загрузке инкрементального снимка
-if ls "${TARGET_DIR}${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
-    echo "Переходим к загрузке инкрементального снимка..."
-    if ls "${TARGET_DIR}${INCREMENTAL_SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
-        echo "Инкрементальный снимок уже существует."
-    else
-        download_file "$INCREMENTAL_SNAPSHOT_URL" "$TARGET_DIR"
-    fi
+# Проверяем наличие инкрементального снимка
+if ls "${TARGET_DIR}/${INCREMENTAL_SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+    echo "Инкрементальный снимок уже существует."
 else
-    echo "Основной снимок отсутствует. Инкрементальный снимок загружаться не будет."
-    exit 1
+    if ls "${TARGET_DIR}/${SNAPSHOT_MASK}" 1>/dev/null 2>&1; then
+        echo "Основной снимок найден. Начинаем загрузку инкрементального снимка..."
+        download_file "http://$ip/incremental-snapshot.tar.bz2" "$TARGET_DIR"
+    else
+        echo "Основной снимок отсутствует. Инкрементальный снимок загружаться не будет."
+        exit 1
+    fi
 fi
 
 # Перезагрузка демонов и перезапуск Solana
